37c412ab1329d84d4b539bdafd10d7177c947146
Elina <1WsiKnLa>
2020-06-04 01:33:20 +0300

    TeachersSection: fix data enter

--- frontend/sections/study/TeachersSection/index.js
+++ frontend/sections/study/TeachersSection/index.js
@@ -1,19 +1,34 @@
 // Vendor
 import React from 'react';
+import PropTypes from 'prop-types';
 // Internal
 import Typography from 'components/Typography';
 import GridRows from 'components/GridRows';
 import style from './style.scss';
 
 const TeachersSection = ({teachers}) => {
-  return (
-    <section className={style.TeachersSection}>
-      <Typography variant="heading2" hasMargin>
-        Наши преподаватели
-      </Typography>
-      <GridRows data={teachers} />
-    </section>
-  );
+    const teachersFormat = values => {
+        return values.map(item => {
+                return ({
+                    image: `${process.env.API_BASE_URL}${item.Photo.url}`,
+                    text: item.Text,
+                })
+            }
+        );
+    };
+
+    return (
+        <section className={style.TeachersSection}>
+            <Typography variant="heading2" hasMargin>
+                Наши преподаватели
+            </Typography>
+            <GridRows data={teachersFormat(teachers)}/>
+        </section>
+    );
+};
+
+TeachersSection.propTypes = {
+    teachers: PropTypes.array.isRequired,
 };
 
 export default TeachersSection;
