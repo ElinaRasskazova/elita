078b31bc2f61af98eebf6e50799cc3240193b4f1
Elina <1WsiKnLa>
2020-06-05 13:27:27 +0300

    add backend

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+      {
+        name: 'elita-beauty-saloon-strapi',
+        script: 'npm',
+        args: 'start',
+        env: {
+          NODE_ENV: 'production',
+        },
+      },
+    ],
+  };
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5f08988e-5c17-4d17-a4e5-f70a26023f4b'
+};
